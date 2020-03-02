32bc4fa36ba2ce081cd62275537fbb52cbd87785
Bryan Knox <bryan@knoxbits.com>
2020-03-02 06:54:20 -0800

    #9 Stub Authorization Service Tests - fails CI

---
+++ OidcApiAuthorization.Tests/OidcApiAuthorizationServiceTests/AuthorizeAsyncTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Options;
+using OidcApiAuthorization;
+using OidcApiAuthorization.Abstractions;
+using TestFixtures.AzureFunctions;
+using Xunit;
+
+namespace OidcApiAuthorizationServiceTests
+{
+    public class AuthorizeAsyncTests
+    {
+        [Fact]
+        public async void Returns_failure_if_bad_Aurthorization_header()
+        {
+            var listLogger = new ListLoggerFixture();
+
+            IOptions<OidcApiAuthorizationSettings> fakeApiAuthorizationSettingsOptions = null;
+
+            IAuthorizationHeaderBearerTokenParser fakeAuthorizationHeaderBearerTokenParser = null;
+
+            IHeaderDictionary httpRequestHeaders = null;
+
+            var service = new OidcApiAuthorizationService(
+                fakeApiAuthorizationSettingsOptions,
+                fakeAuthorizationHeaderBearerTokenParser,
+                jwtSecurityTokenHandlerWrapper: null, // Not accessed in this test.
+                oidcConfigurationManagerFactory: null); // Not accessed in this test.
+
+            ApiAuthorizationResult result = await service.AuthorizeAsync(
+                httpRequestHeaders,
+                listLogger);
+
+            Assert.False(result.Success);
+            
+            Assert.Equal(
+                "Authorization header is missing, invalid format, or is not a Bearer token.",
+                result.FailureReason);
+        }
+    }
+}
---
+++ OidcApiAuthorization.Tests/OidcApiAuthorizationServiceTests/ConstructorTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.Extensions.Options;
+using Xunit;
+
+namespace OidcApiAuthorization.Tests.OidcApiAuthorizationServiceTests
+{
+    public class ConstructorTests
+    {
+        [Fact]
+        public void Constructor_throws_if_missing_settings()
+        {
+            const string expectedExceptionMessage = "some message";
+
+            IOptions<OidcApiAuthorizationSettings> fakeApiAuthorizationSettingsOptions = null;
+            // TODO: FakeApiAuthorizationSettingsOptions class.
+
+            var service = new OidcApiAuthorizationService(
+                fakeApiAuthorizationSettingsOptions,
+                authorizationHeaderBearerTokenParser: null,
+                jwtSecurityTokenHandlerWrapper: null, // Not accessed in this test.
+                oidcConfigurationManagerFactory: null); // Not accessed in this test.
+
+            var ex = Assert.Throws<Exception>(() =>
+                new OidcApiAuthorizationService(
+                    fakeApiAuthorizationSettingsOptions,
+                    authorizationHeaderBearerTokenParser: null, // Not accessed in this test.
+                    jwtSecurityTokenHandlerWrapper: null, // Not accessed in this test.
+                    oidcConfigurationManagerFactory: null) // Not accessed in this test.
+            );
+
+            Assert.Equal(expectedExceptionMessage,  ex.Message);
+        }
+
+    }
+}
